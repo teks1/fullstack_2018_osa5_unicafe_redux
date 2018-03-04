d46f65b1a4273073846bc803ebc6629e693cb56c
teks1 <marko.vuorenmaa@cs.helsinki.fi>
2018-03-04 21:58:19 +0200

    osa5_17_18

--- src/index.js
+++ src/index.js
@@ -1,100 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore} from 'redux'
+import counterReducer from './reducer'
 
-const Button = ({handleClick, text}) => (
-	<button onClick={handleClick}>
-		{text}
-	</button>
-)
+const store = createStore(counterReducer)
 
-const Statistic = ({teksti, numero}) => {
-	return (
-		<tbody>
-		<tr>
-			<td>{teksti}</td>
-			<td>{numero}</td>
-		</tr>
-		</tbody>
-	)
+const Statistiikka = () => {
+	
+  const palautteita = store.getState().good + store.getState().ok + store.getState().bad
+  const positiivisia = store.getState().good / palautteita * 100
+  if (palautteita === 0) {
+    return (
+      <div>
+        <h2>statistiikka</h2>
+        <div>ei yhtään palautetta annettu</div>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>statistiikka</h2>
+      <table>
+        <tbody>
+          <tr>
+            <td>hyvä</td>
+            <td>{store.getState().good}</td>
+          </tr>
+          <tr>
+            <td>neutraali</td>
+            <td>{store.getState().ok}</td>
+          </tr>
+          <tr>
+            <td>huono</td>
+            <td>{store.getState().bad}</td>
+          </tr>
+          <tr>
+            <td>positiivisia</td>
+            <td>{positiivisia} %</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <button onClick={e => store.dispatch({ type: 'ZERO'})}>
+          nollaa
+        </button>
+    </div >
+  )
 }
-
-const Statistics = ({state}) => {
-	if (state.maara === 0) {
-		return (
-			<div>
-				<h2>statistiikka</h2>
-				<p>
-					ei yhtään palautetta
-				</p>
-			</div>
-		)
-	}
-	return (
-		<div>
-			<h2>statistiikka</h2>
-			<table>
-				<Statistic teksti="hyvä" numero={state.hyva}/>
-				<Statistic teksti="neutraali" numero={state.neutraali}/>
-				<Statistic teksti="huono" numero={state.huono}/>
-				<Statistic teksti="keskiarvo" numero={state.asteluku / state.maara}/>
-				<Statistic teksti="positiivisia" numero={state.hyva / state.maara * 100 + ' %'}/>
-			</table>
-		</div>
-	)
-}
-
-
 
 class App extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			hyva: 0,
-			neutraali: 0,
-			huono: 0,
-			maara: 0,
-			asteluku: 0,
-		}
-	}
-	asetaArvo = (arvio, arvo) => {
-		
-		if (arvio === 'neutraali') {
-			return () => {
-				this.setState({neutraali: arvo})
-				this.setState({maara: this.state.maara + 1})
-			}
-		}
-		if (arvio === 'huono') {
-			return () => {
-				this.setState({huono: arvo})
-				this.setState({maara: this.state.maara + 1})
-				this.setState({asteluku: this.state.asteluku - 1})
-			}
-		}
-		return () => {
-			this.setState({hyva: arvo})
-			this.setState({maara: this.state.maara + 1})
-			this.setState({asteluku: this.state.asteluku + 1})
-		}
-	}
-	render() {
-		
-		return(
-			<div>
-				<h2>Anna palautetta</h2>
-				<div>
-					<Button handleClick={this.asetaArvo('hyva', this.state.hyva + 1)}
-					        text="hyvä"/>
-					<Button handleClick={this.asetaArvo('neutraali', this.state.neutraali + 1)}
-					        text="neutraali"/>
-					<Button handleClick={this.asetaArvo('huono', this.state.huono + 1)}
-					        text="huono"/>
-				</div>
-				<Statistics state={this.state}/>
-			</div>
-		)
-	}
+  klik = (nappi) => () => {
+	//console.log(nappi)
+	store.dispatch({ type: nappi})
+  }
+
+  render() {
+	console.log(store.getState())
+    return (
+      <div>
+        <h2>anna palautetta</h2>
+        <button onClick={this.klik('GOOD')}>hyvä</button>
+        <button onClick={this.klik('OK')}>neutraali</button>
+        <button onClick={this.klik('BAD')}>huono</button>
+        <Statistiikka />
+      </div>
+    )
+  }
 }
 
+const renderApp = () => {
+	ReactDOM.render(<App />, document.getElementById('root'))
+  }
+  
+  renderApp()
+  store.subscribe(renderApp)
 
-ReactDOM.render(<App/>, document.getElementById('root'));
---
+++ src/reducer.js
@@ -0,0 +1,29 @@
+const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0
+  }
+  
+  const counterReducer = (state = initialState, action) => {
+    const copy = Object.assign({}, state)
+    switch (action.type) { 
+      case 'GOOD':
+        ++copy.good
+        return copy
+      case 'OK':
+         ++copy.ok
+      return copy
+      case 'BAD':
+        ++copy.bad
+      return copy
+      case 'ZERO':
+        copy.good = 0
+        copy.ok = 0
+        copy.bad = 0
+        return copy
+    }
+    
+    return state
+  }
+  
+  export default counterReducer
